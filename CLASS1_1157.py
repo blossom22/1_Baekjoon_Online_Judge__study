abe24255f18904f415b5253827ed0994e1e6b8dc
blossom22 <ssunyaang@naver.com>
2023-07-19 14:14:52 +0900

    Modified Solution

--- CLASS1_1157.py
+++ CLASS1_1157.py
@@ -1,18 +1,16 @@
 # Baekjoon_CLASS1_1157: 단어 공부
 
 a = {}
-n = input().upper()     # 전부 대문자로 만들면 카운트하기도 쉽다
+n = input().upper()     # 전부 대문자로 바꿔서 생각하면 편하다. 어차피 결과출력도 대문자니까
 for i in n:
     if i not in a:
-        a[i] = 1    
+        a[i]=1
     else:
-        a[i] += 1       # 문자 개수 세서 딕셔너리에 추가한다
+        a[i]+=1     # 딕셔너리 초기 세팅
 
-b = list(a.values())
-max_value = max(b)
-max_keys = [key for key, value in a.items() if value == max_value]
-
-if len(max_keys) > 1:
+max_value = max(a.values())
+max_keys = [key for key,value in a.items() if value == max_value]
+if len(max_keys) >1:
     print('?')
 else:
     print(max_keys[0])
