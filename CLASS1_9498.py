f6d1c51bca381436d328c5e497a8bcd60bdaef6f
blossom22 <ssunyaang@naver.com>
2023-07-20 13:10:51 +0900

    Initial commit

---
+++ CLASS1_9498.py
@@ -0,0 +1,13 @@
+# Baekjoon_CLASS1_9498: 시험 성적
+
+n = int(input())
+if n>=90: 
+    print('A')
+elif n>=80:
+    print('B')
+elif n>=70:
+    print('C')
+elif n>=60:
+    print('D')
+else:
+    print('F')
