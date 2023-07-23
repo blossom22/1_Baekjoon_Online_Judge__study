530f785e30958ad2fd62921ed3e1ba8b1ad93186
blossom22 <ssunyaang@naver.com>
2023-07-23 23:07:52 +0900

    Initial commit

---
+++ 2393.py
@@ -0,0 +1,11 @@
+# Baekjoon_CLASS0_2393: Rook  
+print('  ___  ___  ___')
+print('  | |__| |__| |')
+print('  |           |')
+print('   \_________/')
+print('    \_______/')
+for i in range(4): print('     |     |')
+print('     |_____|')
+print('  __/       \__')
+print(' /             \\')
+print('/_______________\\')
