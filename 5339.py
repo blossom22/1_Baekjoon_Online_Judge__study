55335cada25ad19b368bb9dfa66d52827a3b6dec
blossom22 <ssunyaang@naver.com>
2023-07-24 13:03:54 +0900

    Initial commit

---
+++ 5339.py
@@ -0,0 +1,10 @@
+# Baekjoon_CLASS0_5339: 콜센터 
+
+print('     /~\\')
+print('    ( oo|')
+print('    _\=/_')
+print('   /  _  \\')
+print('  //|/.\|\\\\')
+print(' ||  \ /  ||')
+print('============')
+for i in range(3): print('|          |')
