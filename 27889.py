217f1fb48859590485c9cfcca33c56cf1d32ea1b
blossom22 <ssunyaang@naver.com>
2023-07-26 23:01:03 +0900

    Initial commit

---
+++ 27889.py
@@ -0,0 +1,3 @@
+# Baekjoon_BRONZE5_27889: 특별한 학교 이름 
+a = {'NLCS':'North London Collegiate School', 'BHA':'Branksome Hall Asia', 'KIS':'Korea International School', 'SJA':'St. Johnsbury Academy'}
+print(a[input()])
