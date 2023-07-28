3ec3975ff52f09effb8e363a766a6d9c2d7ea025
blossom22 <ssunyaang@naver.com>
2023-07-28 12:48:40 +0900

    Initial commit

--- 10768.py
+++ 10768.py
@@ -2,9 +2,9 @@
 
 n = int(input())
 m = int(input())
-if n>=2 and m>18:
+if (n>2) or (n==2 and m>18):
     print('After')
-elif n<=2 and m<18:
+elif (n<2) or (n==2 and m<18):
     print('Before')
-else: 
+elif n==2 and m==18:
     print('Special')
