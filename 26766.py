c64639fe6c43c06bd735c769dac5ccab206172d5
blossom22 <ssunyaang@naver.com>
2023-07-28 20:52:17 +0900

    Initial commit

---
+++ 26766.py
@@ -0,0 +1,12 @@
+# Baekjoon_BRONZE5_26766: Serca 
+a = ''' @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     '''
+n = int(input())
+[print(a) for _ in range(n)]
