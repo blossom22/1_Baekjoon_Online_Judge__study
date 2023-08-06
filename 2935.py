84386f1e547e14da2f7fe27647af945fe82e5615
blossom22 <ssunyaang@naver.com>
2023-08-06 09:38:56 +0900

    Initial commit

---
+++ 2935.py
@@ -0,0 +1,11 @@
+# Baekjoon_BRONZE3_2935: 소음   
+
+import sys
+a = int(sys.stdin.readline())
+c = input()
+b = int(sys.stdin.readline())
+
+if c == '+':
+    print(a+b)
+elif c == '*':
+    print(a*b)
