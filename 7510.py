7d15879c9e8b9f9a6d7620f66e1865251a7d5237
blossom22 <ssunyaang@naver.com>
2023-08-11 09:50:26 +0900

    Initial commit

---
+++ 7510.py
@@ -0,0 +1,12 @@
+# Baekjoon_BRONZE4_7510: 고급 수학   
+
+n = int(input())
+m = [list(map(int, input().split())) for _ in range(n)]
+for i in range(1,n+1):
+    data = sorted(m[i-1])
+    if data[0]**2 + data[1]**2 == data[2]**2:
+        print('Scenario #{}:'.format(i), 'yes', sep='\n')
+        print()
+    else:
+        print('Scenario #{}:'.format(i), 'no', sep='\n')
+        print()
