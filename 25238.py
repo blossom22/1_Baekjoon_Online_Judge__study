44f46663b04222d5d644739625634c4a83035c28
blossom22 <ssunyaang@naver.com>
2023-08-15 11:50:38 +0900

    Modified solution

--- 25238.py
+++ 25238.py
@@ -1,5 +1,6 @@
 # Baekjoon_BRONZE4_25238: 가희와 방어율 무시
 
+from decimal import Decimal
 a,b = map(int, input().split())
-bang = a*(1-b/100)
+bang = Decimal(a*((100-b)/100))
 print(1) if bang<100 else print(0)
