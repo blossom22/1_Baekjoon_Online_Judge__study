092454863c6b180e59fb9907050ad449e81ce57e
blossom22 <ssunyaang@naver.com>
2023-08-28 21:53:29 +0900

    initial commit

---
+++ 6903.py
@@ -0,0 +1,10 @@
+# Baekjoon_BRONZE3_6903: Trident   
+
+t = int(input())
+s = int(input())
+h = int(input())
+for i in range(t):
+    print('*'+' '*s+'*'+' '*s+'*')
+print('*'*(2*s+3))
+for i in range(h):
+    print(' '*(s+1)+'*')
