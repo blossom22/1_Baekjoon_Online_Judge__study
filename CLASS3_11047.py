e8dace24c019bcb7848c82ea18852da5b9208e5b
blossom22 <ssunyaang@naver.com>
2023-12-30 14:42:44 +0900

    Renamed

--- CLASS3_11047.py
+++ CLASS3_11047.py
@@ -1,4 +1,4 @@
-# Baekjoon_CLASS2_Silver4_11047: 동전 0
+# Baekjoon_CLASS3_Silver4_11047: 동전 0
 # 그리디 알고리즘 문제이다. 최대한 적은 coin의 개수를 쓰도록 동전가치 리스트(a)에서 역순으로 검사하자
 import sys
 n,k = map(int, sys.stdin.readline().split())
